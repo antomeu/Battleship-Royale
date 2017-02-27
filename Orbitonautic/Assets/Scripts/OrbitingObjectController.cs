00ca87c43ac270a5257b235ea765016a76d28560
Antomeu <anto_meu@yahoo.fr>
2017-02-27 07:54:07 +0100

    Started better code organization

---
+++ Orbitonautic/Assets/Scripts/OrbitingObjectController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class OrbitingObjectController
+    {
+        public bool IsNewForceApplied;
+
+        public void Update()
+        {
+            if (IsNewForceApplied)
+            {
+                CalculateTrajectory();
+            }
+
+            CalculateNextPosition();
+
+        }
+
+        public void CalculateNextPosition()
+        {
+
+        }
+
+        public void CalculateTrajectory()
+        {
+
+        }
+    }
+}
