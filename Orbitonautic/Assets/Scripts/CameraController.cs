159d9290dd1041cabb8dc5eba3b18c68672968a3
antomeu <anto_meu@yahoo.fr>
2017-10-31 00:34:31 +0100

    Some billboard adjustements

--- Orbitonautic/Assets/Scripts/CameraController.cs
+++ Orbitonautic/Assets/Scripts/CameraController.cs
@@ -41,10 +41,10 @@ public class CameraController : MonoBehaviour
     {
         float  velocity = 0f;
         scrollProgress =  Mathf.Clamp(scrollProgress - (Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed * Mathf.Pow(scrollProgress + 0.1f , 2f) ), 0f , 1f) ;
-        zoomTarget = Mathf.Lerp(MinCameraFieldOfView, MaxCameraFieldOfView, scrollProgress);
+        zoomTarget = Mathf.SmoothStep(MinCameraFieldOfView, MaxCameraFieldOfView, scrollProgress);
         Camera.fieldOfView = zoomTarget; // Mathf.SmoothDamp(Camera.fieldOfView, zoomTarget, ref velocity, 0.1f);
 
-        cameraDistanceTarget = Mathf.Lerp(MinCameraDistance, MaxCameraDistance, scrollProgress);
+        cameraDistanceTarget = Mathf.SmoothStep(MinCameraDistance, MaxCameraDistance, scrollProgress);
         transform.localPosition = new Vector3(0, 0, - cameraDistanceTarget); //Mathf.SmoothDamp(transform.position.z, cameraDistanceTarget, ref velocity, 0.1f));
     }
 
--- Orbitonautic/Assets/Scripts/UIBillboard.cs
+++ Orbitonautic/Assets/Scripts/UIBillboard.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,14 @@ public class UIBillboard : MonoBehaviour
 {
     public GameManager GameManager;
 
-	
-	void Update ()
+    public float VerticalViewportPercentage = 0.25f;
+
+    public bool LockXRotation;
+    public bool LockYRotation;
+    public bool LockZRotation;
+
+
+    void Update ()
 	{
         FaceCamera();
 	    ReSizeWithCameraDistance();
@@ -15,15 +22,19 @@ public class UIBillboard : MonoBehaviour
 
     void FaceCamera()
     {
-        transform.LookAt(transform.position + GameManager.CameraController.Camera.transform.rotation * Vector3.forward, GameManager.CameraController.Camera.transform.rotation * Vector3.up);
+        //transform.LookAt(transform.position + GameManager.CameraController.Camera.transform.rotation * Vector3.forward, GameManager.CameraController.Camera.transform.rotation * Vector3.up);
+        //transform.rotation = new Quaternion();
+        var targetCameraRotation = GameManager.CameraController.transform.eulerAngles;
+        targetCameraRotation = new Vector3(Convert.ToInt32(!LockXRotation) * targetCameraRotation.x, Convert.ToInt32(!LockYRotation) * targetCameraRotation.y, Convert.ToInt32(!LockZRotation) * targetCameraRotation.z);
+        transform.eulerAngles = targetCameraRotation;
     }
 
     void ReSizeWithCameraDistance()
     {
         var distance = (transform.position - GameManager.CameraController.transform.position).magnitude;
 
-        var frustumHeight = 2.0f * distance * Mathf.Tan(GameManager.CameraController.Camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        var frustumHeight = 2.0f * distance * Mathf.Tan(GameManager.CameraController.Camera.fieldOfView * 0.5f * Mathf.Deg2Rad); // from https://docs.unity3d.com/Manual/FrustumSizeAtDistance.html
 
-        transform.localScale = 0.5f * frustumHeight / 800 * Vector3.one;
+        transform.localScale = VerticalViewportPercentage * frustumHeight / 800 * Vector3.one;
     }
 }
