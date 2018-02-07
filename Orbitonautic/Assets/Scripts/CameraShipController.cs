b5b5ab342c9484d9e8e4a436dae68be4f84e3ac1
antomeu <anto_meu@yahoo.fr>
2018-02-07 19:40:09 +0100

    Modified Spaceship Camera behavior
    Added GitIgnore

--- Orbitonautic/Assets/Scripts/CameraShipController.cs
+++ Orbitonautic/Assets/Scripts/CameraShipController.cs
@@ -6,9 +6,12 @@ public class CameraShipController : MonoBehaviour
     #region Unity Fields
     [HeaderAttribute("References")]
     public Camera Camera;
+    public Transform SpaceShipTransform;
     [Header("Movement")]
     public float ScrollSpeed = 50f;
-    public float CameraRotateSpeed = 3f;
+    public float CameraPanSpeedZoomedOut = 3f;
+    public float CameraPanSpeedZoomedIn = 0.03f;
+
     [Header("FoV")]
     public float MinCameraFieldOfView = 1;
     public float MaxCameraFieldOfView = 70;
@@ -46,33 +49,26 @@ public class CameraShipController : MonoBehaviour
     #region Private Logic
     void ZoomCamera()
     {
-        float  velocity = 0f;
+        float  velocity = 10f;
         scrollProgress =  Mathf.Clamp(scrollProgress - (Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed * Mathf.Pow(scrollProgress + 0.1f , 2f) ), 0f , 1f) ;
         zoomTarget = Mathf.SmoothStep(MinCameraFieldOfView, MaxCameraFieldOfView, scrollProgress);
-        Camera.fieldOfView = zoomTarget; // Mathf.SmoothDamp(Camera.fieldOfView, zoomTarget, ref velocity, 0.1f);
+        Camera.fieldOfView =  Mathf.SmoothDamp(Camera.fieldOfView, zoomTarget, ref velocity, 0.1f);
 
         cameraDistanceTarget = Mathf.SmoothStep(MinCameraDistance, MaxCameraDistance, scrollProgress);
         //transform.localPosition = new Vector3(0, 0, - cameraDistanceTarget); //Mathf.SmoothDamp(transform.position.z, cameraDistanceTarget, ref velocity, 0.1f));
     }
-
-
-
-
-
- 
+    
     void PanCamera()
     {
 
         if (Input.GetMouseButton(0))//Pan camera
         {
-            transform.Rotate(CameraRotateSpeed* new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")), Space.Self);
-            Debug.Log("Is clicked");
+            var PanSpeed = Mathf.Lerp(CameraPanSpeedZoomedIn, CameraPanSpeedZoomedOut, Mathf.InverseLerp(MinCameraFieldOfView, MaxCameraFieldOfView, Camera.fieldOfView));
+            transform.Rotate(PanSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")), Space.Self);
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, 0);
         }
-        else
-        {
-            //transform.LookAt(Pivot.transform);
-            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Pivot.transform.position - transform.position), 5 * Time.deltaTime);
-        }
+        
+
     }
     #endregion
 }
