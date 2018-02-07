e9a0ef7225fd4d27e810bc8c7cf9fa79449b3af0
antomeu <anto_meu@yahoo.fr>
2018-02-07 23:29:44 +0100

    Overhauled controls, camera and ship animations

--- Orbitonautic/Assets/Scripts/SpaceShipController.cs
+++ Orbitonautic/Assets/Scripts/SpaceShipController.cs
@@ -11,29 +11,51 @@ namespace Assets.Scripts
         #region Unity Fields
         [Header("Ship Properties")]
         public float ThrusterMultiplier = 20;
+        public bool IsFacingSpeedVector;
         [Header("Own References")]
         public Animator SpaceShipAnimator;
         #endregion
 
+        #region Private Fields
+        Vector3 UpVector;
+        #endregion
+
+
         #region Unity Logic
         void Update()
         {
-            transform.forward = Speed; //TODO: set up vector to depend on main gravitational body orientation
+            if (IsFacingSpeedVector)
+                transform.forward = Speed; //TODO: set up vector to depend on main gravitational body orientation
+            Debug.Log(transform.localEulerAngles);
 
-            ThrustersForce = ThrusterMultiplier * (
-                Input.GetAxis("Horizontal") * transform.forward 
-                + Input.GetAxis("Sideway") * transform.right 
-                + Input.GetAxis("Vertical") * transform.up);
-            
+
+            ActivateThrusters();
+            Animate();
+        }
+        #endregion
+
+        #region Private Logic
+
+        void ActivateThrusters()
+        {
+            ThrustersForce = ThrusterMultiplier * (    Input.GetAxis("Horizontal") * transform.forward    + Input.GetAxis("Sideway") * transform.right    + Input.GetAxis("Vertical") * transform.up);
+
+            transform.Rotate(Input.GetAxis("RotateAroundMovementAxis")*Vector3.forward);
+            //UpVector.
+        }
+
+        void Animate()
+        {
             SpaceShipAnimator.SetFloat("forward", Input.GetAxis("Horizontal"));
-            SpaceShipAnimator.SetFloat("backward", - Input.GetAxis("Horizontal"));
+            SpaceShipAnimator.SetFloat("backward", -Input.GetAxis("Horizontal"));
 
             SpaceShipAnimator.SetFloat("up", Input.GetAxis("Vertical"));
-            SpaceShipAnimator.SetFloat("down", - Input.GetAxis("Vertical"));
+            SpaceShipAnimator.SetFloat("down", -Input.GetAxis("Vertical"));
 
             SpaceShipAnimator.SetFloat("left", -Input.GetAxis("Sideway"));
             SpaceShipAnimator.SetFloat("right", Input.GetAxis("Sideway"));
         }
         #endregion
+
     }
 }
