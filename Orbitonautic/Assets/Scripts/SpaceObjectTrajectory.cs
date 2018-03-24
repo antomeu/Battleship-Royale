daeb0f6ffcb6765e018965d8c8452bc17b157459
antomeu <anto_meu@yahoo.fr>
2018-03-24 13:01:08 +0100

    Further custom GL line renderer stuff

---
+++ Orbitonautic/Assets/Scripts/SpaceObjectTrajectory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts 
+{
+    class SpaceObjectTrajectory
+    {
+        Vector3[] Positions;
+
+        public int PositionCount
+        {
+            get
+            {
+                return Positions.Length;
+            }
+            set
+            {
+                Array.Resize(ref Positions, value);
+            }
+        }
+
+        void SetPosition(int i, Vector3 position)
+        {
+            
+        }
+    }
+}
