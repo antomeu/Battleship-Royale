ba2db47600ed62b5d7c0ee965d8c25c915a3fe46
antomeu <anto_meu@yahoo.fr>
2018-04-02 16:20:02 +0200

    Some stuff

--- Orbitonautic/Assets/Scripts/UIBillboard.cs
+++ Orbitonautic/Assets/Scripts/UIBillboard.cs
@@ -3,38 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIBillboard : MonoBehaviour
+namespace Assets.Scripts
 {
-    public GameManager GameManager;
-
-    public float VerticalViewportPercentage = 0.25f;
-
-    public bool LockXRotation;
-    public bool LockYRotation;
-    public bool LockZRotation;
-
-
-    void Update ()
-	{
-        FaceCamera();
-	    ReSizeWithCameraDistance();
-	}
-
-    void FaceCamera()
+    public class UIBillboard : MonoBehaviour
     {
-        //transform.LookAt(transform.position + GameManager.CameraController.Camera.transform.rotation * Vector3.forward, GameManager.CameraController.Camera.transform.rotation * Vector3.up);
-        //transform.rotation = new Quaternion();
-        var targetCameraRotation = GameManager.CameraController.transform.eulerAngles;
-        targetCameraRotation = new Vector3(Convert.ToInt32(!LockXRotation) * targetCameraRotation.x, Convert.ToInt32(!LockYRotation) * targetCameraRotation.y, Convert.ToInt32(!LockZRotation) * targetCameraRotation.z);
-        transform.eulerAngles = targetCameraRotation;
-    }
+        public GameManager GameManager;
 
-    void ReSizeWithCameraDistance()
-    {
-        var distance = (transform.position - GameManager.CameraController.transform.position).magnitude;
+        public float VerticalViewportPercentage = 0.25f;
 
-        var frustumHeight = 2.0f * distance * Mathf.Tan(GameManager.CameraController.Camera.fieldOfView * 0.5f * Mathf.Deg2Rad); // from https://docs.unity3d.com/Manual/FrustumSizeAtDistance.html
+        public bool LockXRotation;
+        public bool LockYRotation;
+        public bool LockZRotation;
 
-        transform.localScale = VerticalViewportPercentage * frustumHeight / 800 * Vector3.one;
+
+        void Update()
+        {
+            FaceCamera();
+            ReSizeWithCameraDistance();
+        }
+
+        void FaceCamera()
+        {
+            //transform.LookAt(transform.position + GameManager.CameraController.Camera.transform.rotation * Vector3.forward, GameManager.CameraController.Camera.transform.rotation * Vector3.up);
+            //transform.rotation = new Quaternion();
+            var targetCameraRotation = GameManager.CameraController.transform.eulerAngles;
+            targetCameraRotation = new Vector3(Convert.ToInt32(!LockXRotation) * targetCameraRotation.x, Convert.ToInt32(!LockYRotation) * targetCameraRotation.y, Convert.ToInt32(!LockZRotation) * targetCameraRotation.z);
+            transform.eulerAngles = targetCameraRotation;
+        }
+
+        void ReSizeWithCameraDistance()
+        {
+            var distance = (transform.position - GameManager.CameraController.transform.position).magnitude;
+
+            var frustumHeight = 2.0f * distance * Mathf.Tan(GameManager.CameraController.Camera.fieldOfView * 0.5f * Mathf.Deg2Rad); // from https://docs.unity3d.com/Manual/FrustumSizeAtDistance.html
+
+            transform.localScale = VerticalViewportPercentage * frustumHeight / 800 * Vector3.one;
+        }
     }
 }
